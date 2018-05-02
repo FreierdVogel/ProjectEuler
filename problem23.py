33290509b76ae6a0d8f66454eb9d4394bebf8058
FreierdVogel <santigu10@hotmail.com>
2018-05-02 22:20:58 +0200

    Still trying 23

--- problem23.py
+++ problem23.py
@@ -1,20 +1,18 @@
 from math import sqrt
-#def fac(n):
-    #sum(k for i in range(1, int(sqrt(n))+1) if n % i == 0 for k in (i, n // i) if k!=n)
+
 def is_abundant(n):
 	return sum([k for i in range(1, int(sqrt(n))+1) if n % i == 0 for k in (i, n // i) if k!=n])>n
 
 print(is_abundant(12))
-v = 30
-j = 0
-for i in range(12,v//2):
-    
-    if is_abundant(i):
-        continue
+v = 28123 
+j = set()
+abun = [x for x in range(12, v+1) if is_abundant(x)]
+for i in range(12,v+1):
     print(i)
-    for n in range(i):
-        #print(n)
-        if not is_abundant(n):
-            print(n, i)
-            j += n
-print(j)
+    for n in abun:
+        if n>i or is_abundant(i-n):
+            break
+        if not is_abundant(i-n):
+            j.add(n)
+            #print(i, n)
+print(sum(j))
