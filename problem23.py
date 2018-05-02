05b21b8f275789bde42a81ad8ce66cf124295165
FreierdVogel <santigu10@hotmail.com>
2018-05-02 15:11:08 +0200

    Intento con comprobaciones

--- problem23.py
+++ problem23.py
@@ -1,24 +1,20 @@
 from math import sqrt
-from time import time
-from itertools import count
+#def fac(n):
+    #sum(k for i in range(1, int(sqrt(n))+1) if n % i == 0 for k in (i, n // i) if k!=n)
+def is_abundant(n):
+	return sum([k for i in range(1, int(sqrt(n))+1) if n % i == 0 for k in (i, n // i) if k!=n])>n
 
-def abundants(n):
-	while n>1:
-		a=0
-		for i in range(1,  n//2+1):
-			if n%i==0:
-				a+=i
-		if a>n:
-			yield n
-		n -= 1
-
-t0 = time()
-v = 28124
-a = [x for x in abundants(v)]
-print("Done,  he tardado", time() - t0)
-cont = 0
-for i in range(12,v):
-    for j in a:
-        if i-j not in a:
-            cont += i
-print(time()-t0, len(a), cont)
+print(is_abundant(12))
+v = 30
+j = 0
+for i in range(12,v//2):
+    
+    if is_abundant(i):
+        continue
+    print(i)
+    for n in range(i):
+        #print(n)
+        if not is_abundant(n):
+            print(n, i)
+            j += n
+print(j)
