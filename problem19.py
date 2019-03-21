d8d5f571f2d0d74b8e50c230757e0570cacfc82e
freierdvogel <santigu10@hotmail.com>
2019-03-21 13:57:51 +0100

    modified

--- problem19.py
+++ problem19.py
@@ -1,4 +1,4 @@
-año = 0
+"""año = 0
 if año%4==0 and  año%100!=0 or año%400==0:
     diasfeb = 29
 else:
@@ -6,4 +6,19 @@ else:
 
 mes = {"jan":31, "feb":diasfeb, "mar":31, "abr":30, "may":31, "jun":30, "jul":31, "ago":31, "sep":30, "oct":31, "nov":30, "dec":31}
 
-for i in range()
+for i in range()"""
+from itertools import count
+days = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
+
+year = 1901
+monthcont = 0
+for i in count(0):
+    if not year%4:
+        if not year%100:
+            if not year%400:
+                febdays = 29
+            febdays = 28
+        febdays=29
+
+    monthdays = [31,febdays,31,30,31,30,31,31,30,31,30,31]
+    print(dias[i%7])
